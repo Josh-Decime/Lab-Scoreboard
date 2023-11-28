73feefee94142444b8d85acfe4d7a18ad00e8561
Josh-Decime <josh_decime@yahoo.com>
2023-11-27 22:33:04 -0700

    functionality is all there, just finishing it up

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ console.log('Hello world')
 
 let teamOne = 0
 let teamTwo = 0
-let scoreBoardOne = ''
+
 
 function scoreOne() {
-    console.log('scoreOne')
-    teamOne += 1
+    console.log('scoreOne');
+    teamOne += 1;
+    document.getElementById('scoreBoardOne').innerText = teamOne;
+}
+
+function threeScoreOne() {
+    teamOne += 3;
+    document.getElementById('scoreBoardOne').innerText = teamOne;
 }
 
 function scoreTwo() {
-    console.log('scoreTwo')
-    teamTwo =+ 1
+    console.log('scoreTwo');
+    teamTwo += 1;
+    document.getElementById('scoreBoardTwo').innerText = teamTwo;
+}
+
+function resetScores() {
+    console.log('clear')
+    teamOne = 0;
+    teamTwo = 0;
+    document.getElementById('scoreBoardOne').innerText = teamOne;
+    document.getElementById('scoreBoardTwo').innerText = teamTwo;
 }
 
 
-document.getElementById('scoreBoardOne').innerText =  teamOne
+// document.getElementById('scoreBoardOne').innerText =  teamOne
